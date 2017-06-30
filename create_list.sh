ba69033b342c73b5e90ae12d06318687dae064f3
sjwang <sjwang_echo@163.com>
2017-06-30 16:27:55 +0800

    Add files via upload

---
+++ create_list.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+root_dir=/home/echo/vision/caffe/data/VOCdevkit/
+sub_dir=ImageSets/Main
+bash_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" #当前sh文件所在的目录
+for dataset in trainval test
+do
+  dst_file=$bash_dir/$dataset.txt
+  if [ -f $dst_file ]
+  then
+    rm -f $dst_file
+  fi
+  for name in VOC2007 VOC2012
+  do
+    if [[ $dataset == "test" && $name == "VOC2012" ]]　#因为VOC2012没有text.txt
+    then
+      continue
+    fi
+    echo "Create list for $name $dataset..."
+    dataset_file=$root_dir/$name/$sub_dir/$dataset.txt
+
+    img_file=$bash_dir/$dataset"_img.txt"
+    cp $dataset_file $img_file
+    sed -i "s/^/$name\/JPEGImages\//g" $img_file #表示在前面插入
+    sed -i "s/$/.jpg/g" $img_file　#表示在后面插入
+
+    label_file=$bash_dir/$dataset"_label.txt"
+    cp $dataset_file $label_file
+    sed -i "s/^/$name\/Annotations\//g" $label_file
+    sed -i "s/$/.xml/g" $label_file
+
+    paste -d' ' $img_file $label_file >> $dst_file
+
+    rm -f $label_file
+    rm -f $img_file
+  done
+
+  # Generate image name and size infomation.
+  if [ $dataset == "test" ]
+  then
+    $bash_dir/../../build/tools/get_image_size $root_dir $dst_file $bash_dir/$dataset"_name_size.txt"
+  fi
+
+  # Shuffle trainval file.洗牌，上面并没有对test.txt文件洗牌
+  if [ $dataset == "trainval" ]
+  then
+    rand_file=$dst_file.random
+    cat $dst_file | perl -MList::Util=shuffle -e 'print shuffle(<STDIN>);' > $rand_file
+    mv $rand_file $dst_file
+  fi
+done
